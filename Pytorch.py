521920680bf9a77e49d3496a0aaa6e97fd780f16
Janan <71359987+Jananzzzz@users.noreply.github.com>
2022-09-08 18:03:37 +0800

    change font

---
+++ Pytorch.py
@@ -0,0 +1,27 @@
+import torch
+print(f"is cuda available for this system?{torch.cuda.is_available()}")
+print(f"cuda version: {torch.version.cuda}")
+print(f"torch version is: {torch.__version__}")
+# welcome to the world of vim.
+# from today i will continue coding with vim.
+# in the windows command line, input: code the-file-you-want-to-code-with-vscode.
+# then it will help you open with vscode.
+# the current font is already beautiful enough.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ import_os.py
@@ -0,0 +1,5 @@
+# importing os module
+import os
+# get the current working directory
+cwd = os.getcwd()
+print(f"the current working directory is: {cwd}")
